7984ba051eca3111a87a8ce3b253bf5a8aec9af5
kyoung-jnn <rudwls468@gmail.com>
2021-09-24 23:07:57 +0900

    feat: 웹팩 설정 추가

--- react-typescript-webpack/webpack.config.js
+++ react-typescript-webpack/webpack.config.js
@@ -1,55 +1,91 @@
 const webpack = require("webpack");
 const path = require("path");
+const sass = require("sass");
+const fibers = require("fibers");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const prod = process.env.NODE_ENV === "production";
+module.exports = (env, argv) => {
+  const prod = argv.mode === "production";
 
-module.exports = {
-  devServer: {
-    historyApiFallback: false,
-    port: 3000,
-    hot: true,
-  },
-  mode: prod ? "production" : "development",
-  devtool: prod ? "hidden-source-map" : "eval",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_module/,
-        use: ["babel-loader", "ts-loader"],
-      },
+  return {
+    devServer: {
+      historyApiFallback: false,
+      port: 3000,
+      hot: true,
+    },
+    mode: prod ? "production" : "development", // mode 설정
+    devtool: prod ? "hidden-source-map" : "eval", // source map 옵션
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path: path.join(__dirname, "/dist"),
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)?$/,
+          exclude: /node_module/,
+          use: ["babel-loader", "ts-loader"],
+        },
+        {
+          test: /\.scss?$/,
+          exclude: /node_module/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: sass, //dart-sass 적용
+                sassOptions: {
+                  fiber: fibers, // fibers 적용, sass 컴파일 속도 향상
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/,
+          exclude: /node_module/,
+          use: {
+            loader: "url-loader",
+            options: {
+              name: "assets/[name].[hash].[ext]",
+              fallback: "file-loader",
+              limit: 5000, // 5kb 미만 파일만 data url로 처리
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: "react", // 자주 사용되는 모듈 지정
+      }),
+      new webpack.HotModuleReplacementPlugin(), // 개발 모드시 코드 핫로드 가능
+      new HtmlWebpackPlugin({
+        template: "./public/index.html", // 템플릿 경로 설정
+        templateParameters: {
+          // 템플릿에 주입할 파라매터 변수 지정
+          env: prod ? "" : "(개발용)",
+        },
+        favicon: "./src/assets/logo.png",
+        minify: prod
+          ? {
+              // 배포 모드시 최적화
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+      }),
+      new CleanWebpackPlugin(), // 새로운 빌드시 이전 빌드 결과물 제거
+      new MiniCssExtractPlugin({ filename: `[name].css` }), // 빌드 결과에서 스타일 분리
     ],
-  },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html", // 템플릿 경로 설정
-      templateParameters: {
-        // 템플릿에 주입할 파라매터 변수 지정
-        env: prod ? "" : "(개발용)",
-      },
-      minify: prod
-        ? {
-            // 운영시 최적화
-            collapseWhitespace: true, // 빈칸 제거
-            removeComments: true, // 주석 제거
-          }
-        : false,
-    }),
-    new webpack.HotModuleReplacementPlugin(), // 개발 모드 핫로드 기능
-    new CleanWebpackPlugin(), // 이전 빌드 결과물 제거
-  ],
+  };
 };
